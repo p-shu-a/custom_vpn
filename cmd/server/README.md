68df160cdbd167ec0e1f719e874e5789d3fb67c7
p-shu-a <180306401+p-shu-a@users.noreply.github.com>
2025-05-19 22:24:33 -0400

    added error reporting via channels. and a context to register shutdowns. there seems to be an issue with the context, because sending a ctrl+c does no longer exit the server. gotta look into that

---
+++ cmd/server/README.md
@@ -0,0 +1,58 @@
+### Adding Contexts
+- What is a context for?
+    - a context carries deadlines, cancellation signals and metadata across go-routines
+    - specifically, you can catch terminations and gracefully shutdown/clean up a go-routine.
+
+- Contexts employ select{}. Select is like a switch statement, except:
+    - there is not conditional
+    - it blocks until one of the case clauses can be executed
+    - re:blocking, thats why select{} blocks indefinitly. though, if you use a default, it executes that operation.
+
+- When do you use a context? do you create a context or accept it?
+    - worker/child/called functions, should accept a context
+    - top-level functions should create context
+    - eg. If main() calls foo(), then main() should create context, and foo() should accept it
+
+- What are some core context functions?
+    - context.Background() is used to creating a context
+    - Context is an interface with the following functions:
+        - Done(), Deadline(), Err(), Value()
+    - You can also create "derived contexts", which are created with parent contexts.
+        - Four: WithCancel(), WithTimeout(), WithDeadline(), WithValue()
+        - `context.WithCancel(parentContext)` this returns a context and a CancelFunc
+            - the cancelFunc must be called to release resources.
+        - Derived contexts can be layered. A context can have a timeout, a value, deadline, etc
+
+- A Note on `WithCancel(Context) (Context, CancelFunc)`
+    - while every context has a .Done(), it is execute by calling the cancelFunc which is returned by WithCancel
+    - without `WithCancel()` you never get back a cancelfunc which can be called.
+    - ctx.Done() returns a channel. 
+        - in the context interface its defined as `Done() <-chan struct{}`
+        - what does that mean? well...s
+
+### Channels
+- Instantiate a channel using `ch := make(chan Type)`
+- two types (sort of...):
+    - bi-directional: can send to and recieve from.
+    - unidirectional: can either only send or recieve. not both.
+- Every channel created is bi-directional. But you constrain its usage (say in a function) to only send or recieve from that channel
+- when you see `Done() <-chan struct{}` more clearly written as `Done() (<-chan struct{})`
+    - its saying "Done() returns a receive only channel of type struct{}"
+    - we pass an empty struct to just send a signal, since it takes-up no memory.
+- Re:Send-only and Receive-only, a better description of each would be:
+    - send-only     : "you can only SEND TO that channel"
+    - recieve-only  : "you can only RECIEVE FROM that channel"
+- The `<-` notation, and its placement, indicates the direction of operation:
+    - `ch <- 5` sends to channel
+    - `foo := <-ch` recieves from that channel
+- By default channels are unbuffered, that is they only accept a send if a recieve is ready.
+    - Channels are blocking
+    - they force syncronization
+    - VERY interesting behaviour!
+- Buffered Channles on the other hand, allow for values to accomulate and then consumed as needed.
+- We want channels because they allow for communication and coordination between go-routines
+    - go routines can't return values. thus, they must use channels 
+
+
+### Managing Shutdowns and Terminations
+- 
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,71 +1,135 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"custom_vpn/tlsconfig"
 	"custom_vpn/tunnel"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
-// we'll fire off main, and use it to parse the port flag
-// -p flag specifies what port listen on
+// helper method to return a context which will track shutdown/terms
+// basically, we're setting up the signal handling
+func setupShutdownHelper() context.Context {
+	ctx, cancel := context.WithCancel(context.Background())
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs // block here until there is something in the channel
+		cancel()
+	}()
+	return ctx
+} // this is pretty fuckin smart
+
+
 func main(){
-	go listenAndServeWithTLS(9001)
-	go listenAndServeNoTLS(9000)
-	select {}
+
+	ctx := setupShutdownHelper()
+	var wg sync.WaitGroup
+	
+	// Shifted strategy: since we have go-routines called by go-routines, there leads to a bastardized mix of error handling
+	// some places i was returning errors, others i was writing to an error channel. nothing worse than a mix.
+	// so, i'll be writing all errors to a channel.
+	// using errgroup package is another approach, but not gonna look into that righ now...
+	errCh := make(chan error)
+	go errorCollector(errCh)
+
+	// since we call both servers in go-routines
+	wg.Add(1)
+	go listenAndServeWithTLS(9001, ctx, errCh, &wg)
+
+	wg.Add(1)
+	go listenAndServeNoTLS(9000, ctx, errCh, &wg)
+
+	wg.Wait()
+	log.Println("All servers closed. Exiting...")
+	close(errCh)
+}
+
+
+func errorCollector(errCh <-chan error){
+	for err := range errCh{
+		log.Printf("ERROR: %v\n", err)
+	}
 }
 
 // Listen for incoming connections
-func listenAndServeWithTLS(port int){
+func listenAndServeWithTLS(port int, ctx context.Context, errCh chan<- error, wg *sync.WaitGroup) {
+	defer wg.Done()
 
 	serverConfig, err := tlsconfig.ServerTLSConfig()
 	if err != nil {
-		log.Fatalf("server: error getting server config: %v", err)
+		errCh <- fmt.Errorf("error getting server config: %v", err)
+		return
 	}
+
 	listener, err := tls.Listen("tcp6", fmt.Sprintf(":%d", port), serverConfig)
-	if err != nil{
-		log.Fatalf("server: error while starting listener on %d:%v", port, err)
+	if err != nil {
+		errCh <- fmt.Errorf("error while starting listener on %d:%v", port, err)
+		return
 	}
 	defer listener.Close()
 	
 	// a for without a condition is go-lang's "while". this will continue until a break, or a return
 	for {
-		clientConn, err := listener.Accept()
-		if err != nil{
-			fmt.Printf("server: unable to accept connection: %v", err)
-			continue
+		select {
+		case <- ctx.Done():
+			return
+		default:
+			clientConn, err := listener.Accept()
+			if err != nil {
+				if _, ok := err.(net.Error); ok {		// Type assert that the error is a net.error type
+					continue
+				}
+				errCh <- fmt.Errorf("unable to accept connection: %v", err)		// if it ain't, return error
+				return
+			}
+			go handleClientConn(clientConn, errCh)
 		}
-		go handleClientConn(clientConn)
 	}
-	
 }
 
-func listenAndServeNoTLS(port int){
+func listenAndServeNoTLS(port int, ctx context.Context, errCh chan<- error, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	listener, err := net.Listen("tcp6", fmt.Sprintf(":%d",port))
 	if err != nil{
-		log.Fatalf("server: error starting listener (on-tls) on %d: %v", port, err)
+		errCh <- fmt.Errorf("server: error starting listener (on-tls) on %d: %v", port, err)
+		return
 	}
 	defer listener.Close()
 
 	for {
-		clientConn, err := listener.Accept()
-		if err != nil{
-			log.Printf("server: unable to accept connection on %d: %v", port, err)
+		select {
+		case <- ctx.Done():
+			return
+		default:
+			clientConn, err := listener.Accept()
+			// this error handling needs to be looked at
+			if err != nil{
+				errCh <- fmt.Errorf("server: unable to accept connection on %d: %v", port, err)
+				return
+			}
+			go handleClientConn(clientConn, errCh)
 		}
-		go handleClientConn(clientConn)
+		
 	}
 }
 
+func handleClientConn(clientConn net.Conn, errCh chan<- error) {
 
-func handleClientConn(clientConn net.Conn){
-	
-	fmt.Printf("server: recieved a conn RemoteAddr: %v\n", clientConn.RemoteAddr())
-	//clientConn.Write([]byte("conn established with server"))
+	log.Printf("Recieved a conn on %v from %v\n", clientConn.LocalAddr(), clientConn.RemoteAddr())
+
 	targetConn, err := net.Dial("tcp", "127.0.0.1:22")
 	if err != nil{
-		log.Fatalf("server: error while connecting to ssh on server: %v \n", err)
+		errCh <- fmt.Errorf("error while connecting to ssh on server: %v", err)
+		return
 	}
 	tunnel.CreateTunnel(targetConn, clientConn)
 }
